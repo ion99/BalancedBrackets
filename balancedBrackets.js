935543ba0a42ff836618660e1963d82156837f0a
Ion Gorincioi <gorincioii@yahoo.com>
2016-09-25 20:23:00 -0700

    Solution completed

---
+++ balancedBrackets.js
@@ -0,0 +1,27 @@
+function isBalanced(str){  
+  let b = /{|\[|\(|}|\]|\)/g;
+  str = str.match(b).join('');
+  let stack = [];
+  let map = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
+      stack.push(str[i]);
+    } else {
+      let last = stack.pop();
+
+      if (str[i] !== map[last]) {
+        return false;
+      }
+    }
+  }
+
+  if (stack.length !== 0) {
+    return false;
+  }
+
+  return true;
+}
